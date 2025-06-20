60697c762b12c729cef79a5e07c4d173a5ea1bf4
Jacobs066 <israelabaefah2005@gmail.com>
2025-06-20 13:58:02 +0000

    Backend file structure

---
+++ Backend/mobileappdemo/mobileappdemo/src/test/java/com/swift/mobileappdemo/MobileappdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift.mobileappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileappdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
